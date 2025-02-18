75fb7e96ff6eee41fb11d0bba51a0ff21b59eaab
Davi <davioliveiraes7@gmail.com>
2025-02-18 18:26:55 -0300

    config: added modifications

--- filme/models.py
+++ filme/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+LISTA_CATEGORIAS = (
+   ("ANALISES", "Análises"),
+   ("PROGRAMACAO", "Programação"),
+   ("APRESENTACAO", "Apresentação"),
+   ("OUTROS", "Outros"),
+)
 
-# criar o filme
-
-# criar os episodios
-
-# criar o usuario
+class Filme(models.Model):
+   titulo = models.CharField(max_length=100)
+   thumb = models.ImageField(upload_to="thumb_filmes")
+   descricao = models.TextField(max_length=1000)
+   categoria = models.CharField(max_length=15, choices=LISTA_CATEGORIAS)
+   visualizacoes = models.IntegerField(default=0)
+   data_criacao = models.DateTimeField(default=timezone.now)
 
